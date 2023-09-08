57160a58c5634313d378a658d362d5d9c2347f2e
KinanthyCn <kinanthycahyaningrum@mail.ugm.ac.id>
2023-09-08 09:09:27 +0700

    first commit

---
+++ resources/views/boom.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contoh</title>
+    <link rel="stylesheet" href="{{ public('css/style.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Selamat Datang di Halaman Contoh</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+                <li><a href="#">Layanan</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <h2>Tentang Kami</h2>
+        <p>Ini adalah bagian tentang kami. Silakan tambahkan teks atau konten lainnya di sini.</p>
+    </section>
+
+    <section>
+        <h2>Layanan</h2>
+        <p>Ini adalah bagian tentang layanan kami. Silakan tambahkan teks atau konten lainnya di sini.</p>
+    </section>
+
+    <section>
+        <h2>Kontak</h2>
+        <p>Silakan hubungi kami melalui alamat dan nomor telepon berikut:</p>
+        <p>Alamat: Jalan Contoh No. 123, Kota Contoh</p>
+        <p>Nomor Telepon: (123) 456-7890</p>
+    </section>
+
+    <footer>
+        <p>Hak Cipta &copy; 2023 Contoh Company</p>
+    </footer>
+</body>
+</html>
